f1c1b786f21714161129f6e75e31e74ee5eb171a
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-03-24 03:31:49 +0900

    Update launchtitle.c

--- source/fbi/action/launchtitle.c
+++ source/fbi/action/launchtitle.c
@@ -21,16 +21,16 @@ static void action_launch_title_update(ui_view* view, void* data, float* progres
         ui_pop();
         info_destroy(view);
 
-        error_display_res(info, task_draw_title_info, res, "Failed to launch title.");
+        error_display_res(info, task_draw_title_info, res, "타이틀 실행에 실패했습니다.");
     }
 }
 
 static void action_launch_title_onresponse(ui_view* view, void* data, u32 response) {
     if(response == PROMPT_YES) {
-        info_display("Launching Title", "", false, data, action_launch_title_update, task_draw_title_info);
+        info_display("타이틀 실행하기", "", false, data, action_launch_title_update, task_draw_title_info);
     }
 }
 
 void action_launch_title(linked_list* items, list_item* selected) {
-    prompt_display_yes_no("Confirmation", "Launch the selected title?", COLOR_TEXT, selected->data, task_draw_title_info, action_launch_title_onresponse);
+    prompt_display_yes_no("확인", "선택된 타이틀을 실행할까요?", COLOR_TEXT, selected->data, task_draw_title_info, action_launch_title_onresponse);
 }
