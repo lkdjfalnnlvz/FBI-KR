108ae3fefcc5e8a6334d1f11acc2151b77066f38
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-03-24 19:25:21 +0900

    Update extsavedata.c

--- source/fbi/extsavedata.c
+++ source/fbi/extsavedata.c
@@ -10,9 +10,9 @@
 #include "task/uitask.h"
 #include "../core/core.h"
 
-static list_item browse_user_save_data = {"유저 세이브 데이터 찾아보기", COLOR_TEXT, action_browse_user_ext_save_data};
-static list_item browse_spotpass_save_data = {"Spotpass 세이브 데이터 찾아보기(한국판 사용 불가)", COLOR_TEXT, action_browse_boss_ext_save_data};
-static list_item delete_save_data = {"세이브 데이터 삭제", COLOR_TEXT, action_delete_ext_save_data};
+static list_item browse_user_save_data = {"유저 저장 데이터 찾아보기", COLOR_TEXT, action_browse_user_ext_save_data};
+static list_item browse_spotpass_save_data = {"Spotpass 저장 데이터 찾아보기", COLOR_TEXT, action_browse_boss_ext_save_data};
+static list_item delete_save_data = {"저장 데이터 삭제", COLOR_TEXT, action_delete_ext_save_data};
 
 typedef struct {
     populate_ext_save_data_data populateData;
@@ -49,35 +49,7 @@ static void extsavedata_action_update(ui_view* view, void* data, linked_list* it
     if(selected != NULL && selected->data != NULL && (selectedTouched || (hidKeysDown() & KEY_A))) {
         void(*action)(linked_list*, list_item*) = (void(*)(linked_list*, list_item*)) selected->data;
 
-        ui_pop();
-        list_destroy(view);
-
-        action(actionData->items, actionData->selected);
-
-        free(data);
-
-        return;
-    }
-
-    if(linked_list_size(items) == 0) {
-        linked_list_add(items, &browse_user_save_data);
-        linked_list_add(items, &browse_spotpass_save_data);
-        linked_list_add(items, &delete_save_data);
-    }
-}
-
-static void extsavedata_action_open(linked_list* items, list_item* selected) {
-    extsavedata_action_data* data = (extsavedata_action_data*) calloc(1, sizeof(extsavedata_action_data));
-    if(data == NULL) {
-        error_display(NULL, NULL, "외부 저장 데이터 작업 데이터를 할당하지 못했습니다.");
-
-        return;
-    }
-
-    data->items = items;
-    data->selected = selected;
-
-    list_display("추가 데이터 작업", "A: 선택, B: 취소(뒤로 가기)", data, extsavedata_action_update, extsavedata_action_draw_top);
+      장 데이터 작업", "A: 선택, B: 취소(뒤로 가기)", data, extsavedata_action_update, extsavedata_action_draw_top);
 }
 
 static void extsavedata_options_add_entry(linked_list* items, const char* name, bool* val) {
@@ -188,14 +160,14 @@ static void extsavedata_update(ui_view* view, void* data, linked_list* items, li
         listData->populateData.items = items;
         Result res = task_populate_ext_save_data(&listData->populateData);
         if(R_FAILED(res)) {
-            error_display_res(NULL, NULL, res, "추가 데이터 목록 채우기를 시작하지 못했습니다.");
+            error_display_res(NULL, NULL, res, "추가 저장 데이터 목록 채우기를 시작하지 못했습니다.");
         }
 
         listData->populated = true;
     }
 
     if(listData->populateData.finished && R_FAILED(listData->populateData.result)) {
-        error_display_res(NULL, NULL, listData->populateData.result, "추가 데이터 목록 채우기에 실패했습니다..");
+        error_display_res(NULL, NULL, listData->populateData.result, "추가 저장 데이터 목록 채우기에 실패했습니다..");
 
         listData->populateData.result = 0;
     }
@@ -255,7 +227,7 @@ static int extsavedata_compare(void* data, const void* p1, const void* p2) {
 void extsavedata_open() {
     extsavedata_data* data = (extsavedata_data*) calloc(1, sizeof(extsavedata_data));
     if(data == NULL) {
-        error_display(NULL, NULL, "추가 데이터를 할당하지 못했습니다.");
+        error_display(NULL, NULL, "추가 저장 데이터를 할당하지 못했습니다.");
 
         return;
     }
@@ -271,5 +243,5 @@ void extsavedata_open() {
     data->sortById = false;
     data->sortByName = true;
 
-    list_display("추가 데이터", "A: 선택, B: 뒤로 가기, X: 새로고침, Select: 설정", data, extsavedata_update, extsavedata_draw_top);
+    list_display("추가 저장 데이터", "A: 선택, B: 뒤로 가기, X: 새로고침, Select: 설정", data, extsavedata_update, extsavedata_draw_top);
 }
