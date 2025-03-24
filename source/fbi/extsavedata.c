92c8b8fbf944509d849d23b7fe91348094e244fc
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-03-24 19:20:27 +0900

    Update extsavedata.c

--- source/fbi/extsavedata.c
+++ source/fbi/extsavedata.c
@@ -77,7 +77,7 @@ static void extsavedata_action_open(linked_list* items, list_item* selected) {
     data->items = items;
     data->selected = selected;
 
-    list_display("외부 세이브 데이터 작업", "A: 선택, B: 취소(뒤로 가기)", data, extsavedata_action_update, extsavedata_action_draw_top);
+    list_display("추가 데이터 작업", "A: 선택, B: 취소(뒤로 가기)", data, extsavedata_action_update, extsavedata_action_draw_top);
 }
 
 static void extsavedata_options_add_entry(linked_list* items, const char* name, bool* val) {
@@ -188,14 +188,14 @@ static void extsavedata_update(ui_view* view, void* data, linked_list* items, li
         listData->populateData.items = items;
         Result res = task_populate_ext_save_data(&listData->populateData);
         if(R_FAILED(res)) {
-            error_display_res(NULL, NULL, res, "외부 세이브 데이터 목록 채우기를 시작하지 못했습니다.");
+            error_display_res(NULL, NULL, res, "추가 데이터 목록 채우기를 시작하지 못했습니다.");
         }
 
         listData->populated = true;
     }
 
     if(listData->populateData.finished && R_FAILED(listData->populateData.result)) {
-        error_display_res(NULL, NULL, listData->populateData.result, "외부 세이브 데이터 목록 채우기에 실패했습니다..");
+        error_display_res(NULL, NULL, listData->populateData.result, "추가 데이터 목록 채우기에 실패했습니다..");
 
         listData->populateData.result = 0;
     }
@@ -255,7 +255,7 @@ static int extsavedata_compare(void* data, const void* p1, const void* p2) {
 void extsavedata_open() {
     extsavedata_data* data = (extsavedata_data*) calloc(1, sizeof(extsavedata_data));
     if(data == NULL) {
-        error_display(NULL, NULL, "외부 세이브 데이터를 할당하지 못했습니다.");
+        error_display(NULL, NULL, "추가 데이터를 할당하지 못했습니다.");
 
         return;
     }
@@ -271,5 +271,5 @@ void extsavedata_open() {
     data->sortById = false;
     data->sortByName = true;
 
-    list_display("외부 세이브 데이터", "A: 선택, B: 뒤로 가기, X: 새로고침, Select: 설정", data, extsavedata_update, extsavedata_draw_top);
+    list_display("추가 데이터", "A: 선택, B: 뒤로 가기, X: 새로고침, Select: 설정", data, extsavedata_update, extsavedata_draw_top);
 }
