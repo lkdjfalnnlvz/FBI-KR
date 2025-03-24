eaf1ee8116dfe3e09c79298633fa021d7a42acb4
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-03-24 16:00:08 +0900

    Update importsecurevalue.c

--- source/fbi/action/importsecurevalue.c
+++ source/fbi/action/importsecurevalue.c
@@ -37,18 +37,18 @@ static void action_import_secure_value_update(ui_view* view, void* data, float* 
     info_destroy(view);
 
     if(R_SUCCEEDED(res)) {
-        prompt_display_notify("Success", "Secure value imported.", COLOR_TEXT, info, task_draw_title_info, NULL);
+        prompt_display_notify("성공", "보안 값을 가져왔습니다.", COLOR_TEXT, info, task_draw_title_info, NULL);
     } else {
-        error_display_res(info, task_draw_title_info, res, "Failed to import secure value.");
+        error_display_res(info, task_draw_title_info, res, "보안 값을 가져오는 데 실패했습니다.");
     }
 }
 
 static void action_import_secure_value_onresponse(ui_view* view, void* data, u32 response) {
     if(response == PROMPT_YES) {
-        info_display("Importing Secure Value", "", false, data, action_import_secure_value_update, task_draw_title_info);
+        info_display("보안 값 가져오기", "", false, data, action_import_secure_value_update, task_draw_title_info);
     }
 }
 
 void action_import_secure_value(linked_list* items, list_item* selected) {
-    prompt_display_yes_no("Confirmation", "Import the secure value of the selected title?", COLOR_TEXT, selected->data, task_draw_title_info, action_import_secure_value_onresponse);
+    prompt_display_yes_no("확인", "선택한 타이틀의 보안 값을 가져올까요?", COLOR_TEXT, selected->data, task_draw_title_info, action_import_secure_value_onresponse);
 }
