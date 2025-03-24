8f01e6eb3a4f7362db5f9f8d1d8f4733c10e0cde
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-03-24 15:56:01 +0900

    Update deletependingtitles.c

--- source/fbi/action/deletependingtitles.c
+++ source/fbi/action/deletependingtitles.c
@@ -63,7 +63,7 @@ static Result action_delete_pending_titles_restore(void* data, u32 index) {
 }
 
 static bool action_delete_pending_titles_error(void* data, u32 index, Result res, ui_view** errorView) {
-    *errorView = error_display_res(data, action_delete_pending_titles_draw_top, res, "Failed to delete pending title.");
+    *errorView = error_display_res(data, action_delete_pending_titles_draw_top, res, "보류 중인 타이틀을 삭제하는 데 실패했습니다.");
     return true;
 }
 
@@ -84,7 +84,7 @@ static void action_delete_pending_titles_update(ui_view* view, void* data, float
         info_destroy(view);
 
         if(R_SUCCEEDED(deleteData->deleteInfo.result)) {
-            prompt_display_notify("Success", "Pending title(s) deleted.", COLOR_TEXT, NULL, NULL, NULL);
+            prompt_display_notify("성공", "보류 중인 타이틀 삭제됨.", COLOR_TEXT, NULL, NULL, NULL);
         }
 
         action_delete_pending_titles_free_data(deleteData);
@@ -106,9 +106,9 @@ static void action_delete_pending_titles_onresponse(ui_view* view, void* data, u
     if(response == PROMPT_YES) {
         Result res = task_data_op(&deleteData->deleteInfo);
         if(R_SUCCEEDED(res)) {
-            info_display("Deleting Pending Title(s)", "Press B to cancel.", true, data, action_delete_pending_titles_update, action_delete_pending_titles_draw_top);
+            info_display("보류 중인 타이틀 삭제 중", "B를 눌러 취소하세요.", true, data, action_delete_pending_titles_update, action_delete_pending_titles_draw_top);
         } else {
-            error_display_res(NULL, NULL, res, "Failed to initiate delete operation.");
+            error_display_res(NULL, NULL, res, "삭제 작업을 시작하는 데 실패했습니다.");
 
             action_delete_pending_titles_free_data(deleteData);
         }
@@ -142,7 +142,7 @@ static void action_delete_pending_titles_loading_update(ui_view* view, void* dat
 
             prompt_display_yes_no("Confirmation", loadingData->message, COLOR_TEXT, loadingData->deleteData, action_delete_pending_titles_draw_top, action_delete_pending_titles_onresponse);
         } else {
-            error_display_res(NULL, NULL, loadingData->popData.result, "Failed to populate pending title list.");
+            error_display_res(NULL, NULL, loadingData->popData.result, "보류 중인 타이틀 목록을 채우는 데 실패했습니다.");
 
             action_delete_pending_titles_free_data(loadingData->deleteData);
         }
@@ -155,7 +155,7 @@ static void action_delete_pending_titles_loading_update(ui_view* view, void* dat
         svcSignalEvent(loadingData->popData.cancelEvent);
     }
 
-    snprintf(text, PROGRESS_TEXT_MAX, "Fetching pending title list...");
+    snprintf(text, PROGRESS_TEXT_MAX, "보류 중인 타이틀 목록을 가져오는 중...");
 }
 
 void action_delete_pending_titles(linked_list* items, list_item* selected, const char* message, bool all) {
@@ -189,7 +189,7 @@ void action_delete_pending_titles(linked_list* items, list_item* selected, const
     if(all) {
         delete_pending_titles_loading_data* loadingData = (delete_pending_titles_loading_data*) calloc(1, sizeof(delete_pending_titles_loading_data));
         if(loadingData == NULL) {
-            error_display(NULL, NULL, "Failed to allocate loading data.");
+            error_display(NULL, NULL, "로딩 데이터를 할당하는 데 실패했습니다..");
 
             action_delete_pending_titles_free_data(data);
             return;
@@ -202,14 +202,14 @@ void action_delete_pending_titles(linked_list* items, list_item* selected, const
 
         Result listRes = task_populate_pending_titles(&loadingData->popData);
         if(R_FAILED(listRes)) {
-            error_display_res(NULL, NULL, listRes, "Failed to initiate pending title list population.");
+            error_display_res(NULL, NULL, listRes, "보류 중인 타이틀 목록 채우기에 실패했습니다.");
 
             free(loadingData);
             action_delete_pending_titles_free_data(data);
             return;
         }
 
-        info_display("Loading", "Press B to cancel.", false, loadingData, action_delete_pending_titles_loading_update, action_delete_pending_titles_loading_draw_top);
+        info_display("로딩 중", "B를 눌러 취소하세요.", false, loadingData, action_delete_pending_titles_loading_update, action_delete_pending_titles_loading_draw_top);
     } else {
         linked_list_add(&data->contents, selected);
 
@@ -221,9 +221,9 @@ void action_delete_pending_titles(linked_list* items, list_item* selected, const
 }
 
 void action_delete_pending_title(linked_list* items, list_item* selected) {
-    action_delete_pending_titles(items, selected, "Delete the selected pending title?", false);
+    action_delete_pending_titles(items, selected, "선택한 보류 중인 타이틀을 삭제할까요?", false);
 }
 
 void action_delete_all_pending_titles(linked_list* items, list_item* selected) {
-    action_delete_pending_titles(items, selected, "Delete all pending titles?", true);
+    action_delete_pending_titles(items, selected, "모든 보류 중인 타이틀을 삭제할까요?", true);
 }
